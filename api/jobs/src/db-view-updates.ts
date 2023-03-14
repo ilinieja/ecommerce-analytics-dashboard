483f2675ad4b7bc34b538211b4f7503ef7e4f397
ilinieja <b2709f1b@gmail.com>
2023-03-14 20:21:36 +0100

    Add day-geo-bucket view

--- api/jobs/src/db-view-updates.ts
+++ api/jobs/src/db-view-updates.ts
@@ -2,9 +2,11 @@ import { Agenda } from "@hokify/agenda";
 
 import dbConnection from "../../utils/dbConnection";
 import { calculateDayStats } from "../../models/day-stats.model";
+import { calculateDayGeoBucketStats } from "../../models/day-geo-bucket-stats.model";
 
 const JOB_NAMES = {
   CALCULATE_DAY_STATS: "CALCULATE_DAY_STATS",
+  CALCULATE_DAY_GEO_BUCKET_STATS: "CALCULATE_DAY_GEO_BUCKET_STATS",
 };
 
 export default async function scheduleDbViewUpdates(agenda: Agenda) {
@@ -16,4 +18,13 @@ export default async function scheduleDbViewUpdates(agenda: Agenda) {
     connection.disconnect();
   });
   await agenda.every("1 day", JOB_NAMES.CALCULATE_DAY_STATS);
+
+  agenda.define(JOB_NAMES.CALCULATE_DAY_GEO_BUCKET_STATS, async (job) => {
+    const connection = await dbConnection();
+
+    await calculateDayGeoBucketStats();
+
+    connection.disconnect();
+  });
+  await agenda.every("1 day", JOB_NAMES.CALCULATE_DAY_GEO_BUCKET_STATS);
 }
---
+++ api/models/day-geo-bucket-stats.model.ts
@@ -0,0 +1,58 @@
+import mongoose from "mongoose";
+import Order, { GeoBucket } from "./order.model";
+
+const DAY_GEO_BUCKET_STATS_COLLECTION_NAME = "day_geo_bucket_stats";
+
+const DayGeoBucketStats = new mongoose.Schema(
+  {
+    _id: { type: String, required: true },
+    date: { type: Date, required: true },
+    geoBucket: { type: String, required: true },
+    revenue: { type: Number, required: true },
+    orders: { type: Number, required: true },
+    averageOrderRevenue: { type: Number, required: true },
+  },
+  { collection: DAY_GEO_BUCKET_STATS_COLLECTION_NAME, versionKey: false }
+);
+
+export async function calculateDayGeoBucketStats() {
+  await Order.aggregate([
+    { $unwind: { path: "$items" } },
+    {
+      $group: {
+        _id: {
+          date: { $dateToString: { format: "%Y-%m-%d", date: "$date" } },
+          geoBucket: "$geoLocation.bucket",
+        },
+        revenue: {
+          $sum: { $multiply: ["$items.quantity", "$items.item.price"] },
+        },
+        orders: { $sum: 1 },
+        averageOrderRevenue: {
+          $avg: { $multiply: ["$items.quantity", "$items.item.price"] },
+        },
+      },
+    },
+    {
+      $addFields: {
+        date: {
+          $dateFromString: {
+            dateString: "$_id.date",
+            format: "%Y-%m-%d",
+          },
+        },
+        geoBucket: "$_id.geoBucket",
+      },
+    },
+    {
+      $merge: {
+        into: DAY_GEO_BUCKET_STATS_COLLECTION_NAME,
+        whenMatched: "replace",
+      },
+    },
+  ]);
+}
+
+// This prevents Mongoose from recompiling the model.
+export default mongoose.models.DayGeoBucketStats ||
+  mongoose.model("DayGeoBucketStats", DayGeoBucketStats);
