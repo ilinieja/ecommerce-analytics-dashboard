9b884c2bd06cd33273096f552406269514535c31
ilinieja <b2709f1b@gmail.com>
2023-04-24 20:02:22 +0200

    Add header shadow on scroll

--- pages/overview/index.tsx
+++ pages/overview/index.tsx
@@ -1,18 +1,28 @@
 import TotalStatsBar from "@/components/TotalStatsBar/TotalStatsBar";
+import { useState } from "react";
+import classNames from "classnames";
 
 import styles from "./index.module.css";
 import BreakdownStatsChart from "@/components/BreakdownStatsChart/BreakdownStatsChart";
 import TopPlatformsList from "@/components/TopPlatformsList/TopPlatformsList";
 import { TopLocationsChart } from "@/components/TopLocationsChart/TopLocationsChart";
 
-// TODO: Detect content scroll and add shadow for the header.
 export default function Overview() {
+  const [isContentScrolled, setIsContentScrolled] = useState(false);
+  const handleContentScroll = (event: React.UIEvent<HTMLElement>) => {
+    setIsContentScrolled(event.currentTarget.scrollTop !== 0);
+  };
+
   return (
     <>
-      <header className={styles.pageHeader}>
+      <header
+        className={classNames(styles.pageHeader, {
+          [styles.scrolled]: isContentScrolled,
+        })}
+      >
         <h1 className={styles.pageTitle}>Overview</h1>
       </header>
-      <section className={styles.pageContent}>
+      <section className={styles.pageContent} onScroll={handleContentScroll}>
         <TotalStatsBar />
         <BreakdownStatsChart />
         <div className={styles.section}>
